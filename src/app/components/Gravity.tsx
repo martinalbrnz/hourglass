98a48bb277a7dcd47d0b9e72461fbbf430a8bd50
martinalbrnz <martinalbor96@gmail.com>
2024-07-03 23:16:40 -0300

    feat: throttle event log

--- src/app/components/Gravity.tsx
+++ src/app/components/Gravity.tsx
@@ -1,4 +1,4 @@
-import { fromEvent, Subscription } from "rxjs";
+import { fromEvent, Subscription, throttleTime } from "rxjs";
 import { createSignal, onCleanup, onMount } from "solid-js";
 
 export default function Gravity() {
@@ -10,9 +10,8 @@ export default function Gravity() {
   const subs$ = new Subscription();
 
   onMount(() => {
-    console.log(window);
     subs$.add(
-      coso.subscribe((click: any) => {
+      coso.pipe(throttleTime(1000 / 60)).subscribe((click: any) => {
         console.log("click!:", click);
         setAngleAlpha(click.alpha);
         setAngleBeta(click.beta);
@@ -35,6 +34,17 @@ export default function Gravity() {
         height: "500px",
       }}
     >
+      <div style={{ display: "flex", "flex-direction": "column" }}>
+        <span style={{ "font-size": "45px" }}>
+          A (verde): {angleAlpha().toFixed(2)}
+        </span>
+        <span style={{ "font-size": "45px" }}>
+          B (azul): {angleBeta().toFixed(2)}
+        </span>
+        <span style={{ "font-size": "45px" }}>
+          G (rojo): {angleGamma().toFixed(2)}
+        </span>
+      </div>
       <div
         style={{
           position: "absolute",
